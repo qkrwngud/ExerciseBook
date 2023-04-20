f2532b40073ce6d0b69eb2323b1860342f1db926
박주형 <105642704+qkrwngud@users.noreply.github.com>
2023-04-20 20:44:54 +0900

    4/20

--- English/ExerciseView.swift
+++ English/ExerciseView.swift
@@ -9,26 +9,27 @@ import SwiftUI
 
 struct ExerciseView: View
 {
-    @State var Lst: [String] = ["A", "B", "C", "D"]
-    @State var LStr: [String] = ["a", "b", "c", "d"]
+    @State var E: [String] = ["coin", "take on", "mass-prodect", "refer to", "put ~ into service", "deciding", "intelligent", "performance", "advertise", "find out", "from then on", "protect", "embarrassing", "feed", "display", "overly", "at that time", "take a look", "no longer", "poison", "grain", "appearance", "crowd", "common practice", "at a later date", "a piece of cake", "argue", "Industrial Revolurion", "ceremonially", "misleading", "convenience", "priest", "wheel", "financially", "for some time", "atmosphere", "support", "inaccurate", "award", "ingredient", "mankind", "contain", "needless", "doubt", "remedy", "responsive", "slice", "gradually", "theatrical", "enemy"]
+    @State var K: [String] = ["(새로운 낱말을)만들다", "(의미를)지니게 되다", "대량 생산하다", "~을 나타내다", "~을 사용하기 시작하다", "결정적인", "똑똑한", "공연", "광고하다", "~을 알아내다", "그때부터", "보호하다", "난처한, 당혹스러운", "먹이다", "내보이다, 전시하다", "지나치게, 몹시", "당시에", "~을 보다", "더이상 ~ 아닌", "독", "곡물", "모습, 외모", "군중", "일반적인 습관, 흔한 일", "나중에, 후일에", "식은 죽 먹기", "주장하다", "산업 혁명", "의식적으로, 형식적으로", "오해의 소지가 있는", "편의 편리", "사제, 성직자", "바퀴", "재정적으로", "얼마 동안", "분위기", "부양하다, 지원하다", "부정확한", "수여하다", "재료, 성분", "인류", "포함하다, 들어 있다", "불필요한", "의심, 의혹", "처리 방안, 해결책", "즉각 반응하는", "얇게 썬 조각", "서서희, 점차", "연극의, 공연의", "적"]
     @State var InputLst: [String] = ["", "", "", "", ""]
     @State var InputLStr: [String] = ["", "", "", "", ""]
     
     var body: some View
     {
-        VStack{
-            NavigationView{
+        VStack
+        {
+            NavigationView
+            {
                 Form
                 {
-                    ForEach(0..<Lst.count)
+                    ForEach(0..<E.count)
                     { Index in
                         
                         Section()
                         {
-                            Text("\(Lst[Index])")
-                            Text("\(LStr[Index])")
+                            Text("\(E[Index])")
+                            Text("\(K[Index])")
                         }
-                        
                     }
                 }
             }.navigationTitle("ExerciseView")
--- English/TestView.swift
+++ English/TestView.swift
@@ -12,9 +12,11 @@ import SwiftUI
 struct TestView: View
 {
     
-    @State var Lst: [String] = ["A", "B", "C", "D"]
-    @State var LStr: [String] = ["에이", "비", "씨", "디"]
-    @State var InputStr: [String] = ["", "", "", "", ""]
+    @State var E: [String] = ["coin", "take on", "mass-prodect", "refer to", "put ~ into service", "deciding", "intelligent", "performance", "advertise", "find out", "from then on", "protect", "embarrassing", "feed", "display", "overly", "at that time", "take a look", "no longer", "poison", "grain", "appearance", "crowd", "common practice", "at a later date", "a piece of cake", "argue", "Industrial Revolurion", "ceremonially", "misleading", "convenience", "priest", "wheel", "financially", "for some time", "atmosphere", "support", "inaccurate", "award", "ingredient", "mankind", "contain", "needless", "doubt", "remedy", "responsive", "slice", "gradually", "theatrical", "enemy"]
+    @State var K: [String] = ["만들다", "지니게 되다", "대량 생산하다", "~을 나타내다", "~을 사용하기 시작하다", "결정적인", "똑똑한", "공연", "광고하다", "~을 알아내다", "그때부터", "보호하다", "난처한, 당혹스러운", "먹이다", "내보이다, 전시하다", "지나치게, 몹시", "당시에", "~을 보다", "더이상~아닌", "독", "곡물", "모습, 외모", "군중", "일반적인 습관, 흔한 일", "나중에, 후일에", "식은 죽 먹기", "주장하다", "산업 혁명", "의식적으로, 형식적으로", "오해의 소지가 있는", "편의 편리", "사제, 성직자", "바퀴", "재정적으로", "얼마 동안", "분위기", "부양하다, 지원하다", "부정확한", "수여하다", "재료, 성분", "인류", "포함하다, 들어 있다", "불필요한", "의심, 의혹", "처리 방안, 해결책", "즉각 반응하는", "얇게 썬 조각", "서서희, 점차", "연극의, 공연의", "적"]
+    @State var InputStr: [String] = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+    
+    @State var RandomNumber: [Int] = []
     
     @State var Count: Int = 0
     
@@ -26,22 +28,24 @@ struct TestView: View
             {
                 Form
                 {
-                    ForEach(0..<Lst.count)
+                    ForEach(0..<10)
                     { Index in
                         
                         Section
                         {
                             VStack
                             {
-                                Text("\(Lst[Index])")
-                                TextField("\(Lst[Index])", text: $InputStr[Index])
+                                Text("\(E[Index])")
+                                TextField("\(E[Index])", text: $InputStr[Index])
                                     .multilineTextAlignment(.center)
                                 
-                                if( LStr[Index] == InputStr[Index])
+                                if( K[Index] == InputStr[Index])
                                 {
+                                    Text("✅")
                                     
                                 }
                             }
+                            
                         }
                         
                     }
